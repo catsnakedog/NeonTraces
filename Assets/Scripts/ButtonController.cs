17e8581474574f303acf2c13d2131bd1ba5bcb43
LeeYuChan2020 <73946733+LeeYuChan2020@users.noreply.github.com>
2023-05-04 18:03:13 +0900

    UI초안
    UI 씬에서 StartCanvas와 StageCanvas 제작하여 초안 진행 중입니다.
    Script의 ButtonController은 StartCanvas내의 버튼 함수 작성해두었습니다.

---
+++ Assets/Scripts/ButtonController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonController : MonoBehaviour
+{
+
+    public GameObject menuPanel;
+    private void Start()
+    {
+        menuPanel.SetActive(false); //게임 시작시 메뉴 패널 비활성화
+    }
+
+
+    public void SettingIn() //세팅 메뉴 열기
+    {
+        menuPanel.SetActive(true);
+        
+    }
+    public void SettingOut() //세팅 메뉴 닫기
+    {
+        menuPanel.SetActive(false);
+    }
+
+    public void GameExit() //게임 종료
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+
+    public void StageMenu() //StartCanvas에서 StageCanvas로
+    {
+        Debug.Log("StageMenu");
+    }
+
+}
