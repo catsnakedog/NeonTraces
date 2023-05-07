fb26f2bf50e997d72e23cd882c6fa5f13ee852b2
Mental1107 <mslee020807@gmail.com>
2023-05-07 10:06:26 +0900

    카메라 고정/확대/축소
    Camerafix 스크립트에서 camsize,camspeed 이용해서 줌인,줌아웃과 트래킹 구현

---
+++ Assets/Scripts/CameraAction/CameraFIx.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class Camerafix : MonoBehaviour
+{
+    public static float camsize = 5;
+    public GameObject A;
+    Transform AT;
+    public static float camspeed;
+    void Start()
+    {
+        AT = A.transform;
+    }
+    void LateUpdate()
+    {
+        transform.position = new Vector3(AT.position.x, AT.position.y, transform.position.z);
+        Camera.main.orthographicSize = Mathf.Lerp(Camera.main.orthographicSize, camsize, camspeed);
+    }
+}
+// 전역변수 camsize 이용해서 매 업데이트 마다 해당 사이즈로 변경
+// 더 빠른 확대, 축소 이펙트 원할 시 camspeed도 함께 조정
