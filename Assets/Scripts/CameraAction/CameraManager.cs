50c1b492ccf75efb322dd7ab6909156a412b3cb1
Mental1107 <mslee020807@gmail.com>
2023-05-11 23:54:44 +0900

    zoomin/zoomout 함수 추가

--- Assets/Scripts/CameraAction/CameraManager.cs
+++ Assets/Scripts/CameraAction/CameraManager.cs
@@ -5,10 +5,11 @@ using UnityEngine;
 public class CameraManager : MonoBehaviour
 {
     Shake shake;
+    DataManager Data;
 
     void Start()
     {
-            
+        Data = DataManager.data;
     }
     public void CameraAction(string actionName)
     {
@@ -19,5 +20,15 @@ public class CameraManager : MonoBehaviour
     {
         shake.Shakecamera();
         yield return new WaitForSeconds(0f);
+    }
+    IEnumerator ZoomInAction()
+    {
+        Data.saveData.gameData.camsize += 1;
+        yield return null;
+    }
+    IEnumerator ZoomOutAction()
+    {
+        Data.saveData.gameData.camsize -= 1;
+        yield return null;
     }
 }
